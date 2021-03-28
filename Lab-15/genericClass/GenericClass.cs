db641bbc016a79db9e52eb58272f66d7ed766a02
meenakshirana8587 <69763066+meenakshirana8587@users.noreply.github.com>
2021-03-28 20:04:12 +0530

    generic class

---
+++ Lab-15/genericClass/GenericClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace genericClass
+{
+    class GenericClass<T>
+    {
+        public void display(T item)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+}
---
+++ Lab-15/genericClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace genericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            GenericClass<Product> gn = new GenericClass<Product>();
+            Product p = new Product()
+            { 
+                Id= 1,
+                Name= "pen",
+                Price=30
+            };
+
+            gn.display(p);
+
+            
+
+           
+
+
+
+            
+                
+        }
+    }
+}
