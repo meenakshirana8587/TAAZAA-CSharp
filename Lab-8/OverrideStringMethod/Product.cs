53ff1ad2a3246a7123e9cfcf59861f2f8ab7d45c
meenakshirana8587 <69763066+meenakshirana8587@users.noreply.github.com>
2021-03-28 06:28:24 -0700

    Overriding tostring method

---
+++ Lab-8/OverrideStringMethod/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverrideStringMethod
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            string s = this.Id + " " + this.Name + " " + this.Price;
+            
+            return s;
+        }
+
+    }
+}
---
+++ Lab-8/OverrideStringMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OverrideStringMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product()
+            {
+                Id = 1,
+                Name = "Pen",
+                Price = 200
+            };
+
+            Console.WriteLine(p.ToString());
+
+    }
+    }
+}
