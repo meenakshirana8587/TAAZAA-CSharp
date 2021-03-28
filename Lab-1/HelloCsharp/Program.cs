0227135e85233444df5ab510c27e6a87f4679062
meenakshirana8587 <69763066+meenakshirana8587@users.noreply.github.com>
2021-03-28 05:20:02 -0700

    Hello World

---
+++ Lab-1/HelloCsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($" Hello {name} on {date}");
+        }
+    }
+}
