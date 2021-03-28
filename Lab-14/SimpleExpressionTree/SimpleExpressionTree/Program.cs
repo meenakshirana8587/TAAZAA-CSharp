a31578eb361de6bfee2fb636c8ee2a1bcd6cb752
meenakshirana8587 <69763066+meenakshirana8587@users.noreply.github.com>
2021-03-28 06:57:39 -0700

    Simple expression tree- lambda expressions

---
+++ Lab-14/SimpleExpressionTree/SimpleExpressionTree/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace SimpleExpressionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Func<int, int> square = (x) => x * x;
+
+            //Console.WriteLine(square(7));
+            //System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+            //var a = e.Compile();
+            //Console.WriteLine(a(7));
+
+            //int[] numbers = { 2, 3, 45, 90 };
+
+            List<Product> products = new List<Product>()
+            {
+            new Product()
+            {
+                Id = 1,
+                Title = "Book",
+                Price = 400,
+                Owner = "abc"
+            },
+            new Product()
+            {
+                Id = 2,
+                Title = "Bat",
+                Price = 200,
+                Owner = "pqr"
+            },
+            new Product()
+            {
+                Id = 3,
+                Title = "Book",
+                Price = 400,
+                Owner = "xyz"
+            }
+        };
+
+        
+
+        //var owner = (from p in products where p.Price > 300 select p.Owner).ToList();
+
+        //    owner.ForEach(i => Console.WriteLine(i));
+
+            var ownerId = (from p in products where p.Price > 300 select (p.Owner, p.Id)).ToList();
+            ownerId.ForEach(i => Console.WriteLine(i));
+
+            var owner = products.Where(p => p.Price > 300).Select(p => p.Owner).ToList();
+            owner.ForEach(i => Console.WriteLine(i));
+
+
+
+
+
+
+
+            //Func<Product,Product> owner = () => 
+
+
+        }
+    }
+}
